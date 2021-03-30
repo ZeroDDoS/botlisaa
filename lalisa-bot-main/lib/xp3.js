311ec11fbf72edaab1b21cf338ccf237f507c7e4
ZeroDDoS <64290131+ZeroDDoS@users.noreply.github.com>
2021-03-30 12:19:19 +0700

    Add files via upload

---
+++ lalisa-bot-main/lib/xp3.js
@@ -0,0 +1,23 @@
+exports.xp3 = (id, XPTN, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwa, youtube) => {
+	return `
+
+┏ ❣ *Edukasi Menu* ❣
+┃
+┠❥ *.quran*
+┠❥ *.sholat* <daerah>
+┠❥ *.wiki* <anjing>
+┠❥ *.wikien* <dog>
+┠❥ *.pantun*
+┠❥ *.katabijak*
+┠❥ *.quotes*
+┠❥ *.quotes1*
+┠❥ *.cerpen*
+┠❥ *.puisi1*
+┠❥ *.puisi2*
+┠❥ *.bucin*
+┠❥ *.resep*
+┠❥ *.bitly*
+┠❥ *.hostsearch*
+╿
+╰╼❥  Jangan spam! jeda command 5 detik.`
+}
